c1229a1456f7b59f8e768407a9d0f41d5bc10405
fightman <grandy@citypolyhs.org>
2017-01-11 21:11:14 +0000

    message

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Write your function calls below this line
+setColor(0, 0, "purple");
+setColor(0, 9, "yellow");
+setColor(9, 0, "blue");
+setColor(9, 9, "green");
+setColor(0, 2, "black");
+setColor(0, 06, "black");
+setColor(4, 4, "turquoise");
+setColor(5, 3, "turquoise");
+setColor(5, 5, "turquoise");
+setColor(3, 3, "turquoise");
+setColor(3, 5, "turquoise");
+setColor(6, 2, "turquoise");
+setColor(2, 2, "turquoise");
+setColor(2, 6, "turquoise");
+setColor(6, 6, "turquoise")
